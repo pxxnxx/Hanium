33ddd8d45a27e5031a51b95821d5c97de098fe31
pxxnxx <kmin9811@naver.com>
2021-07-05 20:23:25 +0900

    07/05 20:21 2 page layout activity, vertical scroll

---
+++ app/src/main/java/com/example/test_ttokshow/TotalReviewActivity.java
@@ -0,0 +1,17 @@
+package com.example.test_ttokshow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class TotalReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.total_review_);
+    }
+}
