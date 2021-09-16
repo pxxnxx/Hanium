77ef3da6f6de0aa9f63d7e8f57de44d082d6155e
pxxnxx <kmin9811@naver.com>
2021-09-17 04:20:23 +0900

    firebase

--- 0_MAIN/FireBasePushAlarm.py
+++ 0_MAIN/FireBasePushAlarm.py
@@ -1,7 +1,7 @@
 from pyfcm import FCMNotification
 
-APIKEY = "AAAA0brfvYU:APA91bG31YupO8cjeVl669-duqw_kjRE1XSUdLl0dS9PhPL9YDN1D164W6Hurr9JCUbWgK5Wa9WZ4luNt9UMcccP0XVlNMgvfyaiudeAQcHscCaIu7AZAuB5wZna1uUfr8mM66whRKqx"
-TOKEN = "duVCxvvKRHetPGmc-qO-ti:APA91bGX4POqrviJtSyK8yrtsKeam7tMWvsbd8f_sQjXIHxJYTD8xwF8jjnRDeXRvJoICVO6w72TVW2ZZDJ-rYsyLvu-agWx3kPddsJ-8ND0LArg16h7QvOf9s83ur_oqiCsvX2SlCbx"
+APIKEY = ""
+TOKEN = ""
 push_service = FCMNotification(APIKEY)
 
 
